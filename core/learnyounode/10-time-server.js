2d7add5fb4d1391e4a8d268f52f7c804acbc1d73
André Knörig <andre.knoerig@ixds.de>
2014-09-15 21:05:15 +0200

    lesson 10

--- core/learnyounode/10-time-server.js
+++ core/learnyounode/10-time-server.js
@@ -3,19 +3,30 @@ var net = require('net');
 var server = net.createServer(function (socket) {
 
 	var date = new Date();
+	
 
-	socket.write(date.getFullYear());
+	socket.write(date.getFullYear().toString());
 	socket.write('-');
-	socket.write(date.getMonth());
+	socket.write(leadingZero((date.getMonth()+1).toString()));
 	socket.write('-');
-	socket.write(date.getDate());
+	socket.write( leadingZero( date.getDate().toString() ) );
 	socket.write(' ');
-	socket.write(date.getHours());
+	socket.write(leadingZero(date.getHours().toString()));
 	socket.write(':');
-	socket.write(date.getMinutes());
-	socket.end('\n');
+	socket.write(leadingZero(date.getMinutes().toString()));
+	socket.write('\n');
+
+	socket.end();
 
 
-})
+});
 
 server.listen(process.argv[2]);
+
+
+function leadingZero(str) {
+	if (str.length === 1)
+		return '0' + str;
+	else
+		return str;
+}
